a5d7a645f797a0d6dea7fae6c56f32b7077c7acd
umumararungu <umcynthi@gmail.com>
2023-11-01 18:10:32 +0200

    task

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -19,11 +19,11 @@ if __name__ == '__main__':
 
     employee_name = user_info["name"]
     task_completed = list(filter(lambda obj:
-                                (obj['completed'] is true), todos_info))
+                                (obj['completed'] is True), todos_info))
     total_completed_tasks = len(task_completed)
     total_tasks = len(todos_info)
 
     print("Employee {} is done with tasks ({}/{}):"
-          .format(employee_name,total_completed_tasks,total_tasks))
+          .format(employee_name, total_completed_tasks, total_tasks))
 
     [print("\t " + task["title"]) for task in task_completed]
