779525d36313a91fafe17e07027e91559325d36b
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-11-04 21:21:21 +0200

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """a Python script to export data in the CSV format"""
 
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
