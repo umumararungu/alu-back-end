63593a5ad888f058e30072cddb342589346acc3e
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-11-04 20:41:13 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,23 +1,55 @@
 #!/usr/bin/python3
 """
-    python script that exports data in the JSON format
+Using what you did in the task #0,
+extend your Python script to export
+data in the JSON format.
+
+Requirements:
+
+Records all tasks from all employees
+Format must be: { "USER_ID": [ {"username":
+"USERNAME", "task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS}, {"username": "USERNAME",
+"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
+... ], "USER_ID": [ {"username": "USERNAME", "task":
+"TASK_TITLE", "completed": TASK_COMPLETED_STATUS},
+{"username": "USERNAME", "task": "TASK_TITLE",
+"completed": TASK_COMPLETED_STATUS}, ... ]}
+
+File name must be: todo_all_employees.json
 """
-import json
-import requests
-
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
-    """
-        export to JSON
-    """
-
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    import json
+    import requests
+    import sys
+    # create your api urls used for making the request
+    url1 = f"https://jsonplaceholder.typicode.com/users"
+    url2 = f"https://jsonplaceholder.typicode.com/todos"
+    req_rep1 = requests.get(url1)
+    req_rep2 = requests.get(url2)
+    req_rep1 = req_rep1.json()
+    req_rep2 = req_rep2.json()
+    # get the todo keys for the users
+    all_user_keys = req_rep2[0].keys()
+    # create a dict for storing all users
+    # username, title as task, completed
+    all_user_json = {}
+    user_count = 0
+    each_user_todo = 20
+    for i in range(0, len(req_rep2), each_user_todo):
+        new_list = []
+        for j in range(i, i+each_user_todo):
+            new_dict = {}
+            new_dict["username"] = req_rep1[user_count]["username"]
+            for key in all_user_keys:
+                if key != "title" and key != "completed":
+                    continue
+                if key == "title":
+                    new_dict["task"] = req_rep2[j][key]
+                else:
+                    new_dict["completed"] = req_rep2[j][key]
+            new_list.append(new_dict)
+        all_user_json[f'{req_rep1[user_count]["id"]}'] = new_list
+        user_count += 1
+    with open("todo_all_employees.json", 'w', encoding="utf-8") as jsonfile:
+        json.dump(all_user_json, jsonfile)
